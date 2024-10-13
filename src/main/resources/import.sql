34acae25c2bd87de97091bb3dfb4c8a465c6bad3
laiscrz <lais.albino2003@gmail.com>
2024-10-13 11:24:10 -0300

    feat: updated import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,20 +1,19 @@
 INSERT INTO music (ano_lancamento, album, artista, genero, titulo, album_art_url) VALUES (1996, 'All Eyez on Me', '2Pac', 'HIP_HOP', 'California Love', 'https://abrir.link/ZjOqm');
-INSERT INTO music (ano_lancamento, album, artista, genero, titulo, album_art_url) VALUES (2020, 'Future Nostalgia', 'Dua Lipa', 'Pop', 'Dont Start Now', 'https://abrir.link/fltfI');
+INSERT INTO music (ano_lancamento, album, artista, genero, titulo, album_art_url) VALUES (2020, 'Future Nostalgia', 'Dua Lipa', 'POP', 'Dont Start Now', 'https://abrir.link/fltfI');
 INSERT INTO music (ano_lancamento, album, artista, genero, titulo, album_art_url) VALUES (2019, 'Lover', 'Taylor Swift', 'POP', 'You Need to Calm Down', 'https://abrir.link/MVajI');
 INSERT INTO music (ano_lancamento, album, artista, genero, titulo, album_art_url) VALUES (2018, 'Scorpion', 'Drake', 'HIP_HOP', 'In My Feelings', 'https://abrir.link/cxgTc');
 INSERT INTO music (ano_lancamento, album, artista, genero, titulo, album_art_url) VALUES (2022, 'Planet Her', 'Doja Cat', 'POP', 'Kiss Me More', 'https://abrir.link/xRmKc');
 
 
-INSERT INTO playlist (data_criacao, nome) VALUES (CURRENT_DATE, 'Minhas Favoritas');
-INSERT INTO playlist (data_criacao, nome) VALUES (CURRENT_DATE, 'Para Relaxar');
-INSERT INTO playlist (data_criacao, nome) VALUES (CURRENT_DATE, 'Workout Playlist');
-INSERT INTO playlist (data_criacao, nome) VALUES (CURRENT_DATE, 'Top Hits 2023');
 
-INSERT INTO playlist_music (music_id, playlist_id) VALUES (1, 1);  -- Love Story na playlist "Minhas Favoritas"
-INSERT INTO playlist_music (music_id, playlist_id) VALUES (2, 1);  -- Don't Start Now na playlist "Minhas Favoritas"
-INSERT INTO playlist_music (music_id, playlist_id) VALUES (3, 2);  -- You Need to Calm Down na playlist "Para Relaxar"
-INSERT INTO playlist_music (music_id, playlist_id) VALUES (4, 3);  -- In My Feelings na playlist "Workout Playlist"
-INSERT INTO playlist_music (music_id, playlist_id) VALUES (5, 4);  -- Kiss Me More na playlist "Top Hits 2023"
-INSERT INTO playlist_music (music_id, playlist_id) VALUES (1, 4);  -- Love Story na playlist "Top Hits 2023"
-INSERT INTO playlist_music (music_id, playlist_id) VALUES (2, 3);  -- Don't Start Now na playlist "Workout Playlist"
+INSERT INTO playlist (data_criacao, nome) VALUES (SYSDATE, 'Minhas Favoritas');
+INSERT INTO playlist (data_criacao, nome) VALUES (SYSDATE, 'Para Relaxar');
+INSERT INTO playlist (data_criacao, nome) VALUES (SYSDATE, 'Workout Playlist');
+INSERT INTO playlist (data_criacao, nome) VALUES (SYSDATE, 'Top Hits 2023');
+
+INSERT INTO playlist_music (music_id, playlist_id) VALUES (1, 1);
+INSERT INTO playlist_music (music_id, playlist_id) VALUES (2, 1);
+INSERT INTO playlist_music (music_id, playlist_id) VALUES (3, 2);
+INSERT INTO playlist_music (music_id, playlist_id) VALUES (4, 3);
+INSERT INTO playlist_music (music_id, playlist_id) VALUES (5, 4);
 
